893e6e187942b7633f6a391307c4f96191ef0f9c
AshirArfan <u2022124@giki.edu.pk>
2025-07-24 12:38:53 +0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+      .then(res => setData(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from PostgreSQL</h1>
+      <ul>
+        {data.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
